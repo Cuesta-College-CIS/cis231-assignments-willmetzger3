878ba971b58d8f7b1b4ef19e01dd3b86281a39fe
Your Name <you@example.com>
2022-04-03 11:15:11 -0700

    finalChanges

--- quiz1.java
+++ quiz1.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class quiz1 {
 	public static void main(String[] args) {
 		final int CNT = 5;
-		int minval=100, minidx=0, tmp=0,i,j;
+		int minval=100, minidx=0, tmp=0,i,j,f;
  
 		Scanner scnr = new Scanner(System.in);
 		int N = scnr.nextInt();
 		int[] numbers = new int[N];
 		for (i = 0; i < N; i++)
 			numbers[i] = scnr.nextInt();
-        for (j = 0; j < CNT; j++){
+        for (j = 0; j < CNT; j++)
+        {
             minval = 100;
             for (i = j; i < N; i++)
                 if (numbers[i]<minval) 
@@ -24,7 +25,11 @@ public class quiz1 {
                 }
             numbers[j] = minval;
             numbers[minidx] = tmp;
-            }
+            for (f = 0; f < N; f++)
+                System.out.printf("%d ",numbers[f]);
+            System.out.println();
+
+        }
                 /*
                     minidx = i;
                     minval = numbers[i];
@@ -39,8 +44,7 @@ public class quiz1 {
         System.out.println(tmp);
         System.out.println();
         */
-		for (i = 0; i < N; i++)
-            System.out.println(numbers[i]);
+		
 
 		scnr.close();
 	}
