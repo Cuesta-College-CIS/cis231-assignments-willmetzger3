bbab576a8f7f42ef4698f0a1878c2af4b2790903
Your Name <you@example.com>
2022-05-19 11:14:32 -0700

    final2

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,30 +6,6 @@ import java.util.Arrays;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Boat fastBoat = new Boat(2);
-fastBoat.setName("Firebird");
-System.out.println(fastBoat);
+   
         
-    }
-}
-        public class Vehicle {
-            protected String vehicleName;
-            public void setName(String name) {
-               vehicleName = name;
-            }
-            public String toString() {
-               return vehicleName;
-            }
-         }
-         public class Boat extends Vehicle {
-            private int numEngines;
-            public Boat(int num) {
-               numEngines = num;
-            }
-            public String toString() {
-               return super.toString() + " " + numEngines;
-            } 
-         }
-        
-
     
